237857357c3359153ee5a7d95145e099b5754697
Sanjai R <62475598+Sanjai-R@users.noreply.github.com>
2021-07-26 14:27:32 +0530

    Delete fetchApi.jsx

--- src/Components/fetchApi.jsx
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-const url =
-  " https://api.coingecko.com/api/v3/coins/markets?vs_currency=inr&order=market_cap_desc&per_page=100&page=1&sparkline=false";
-export const fetchApi = async () => {
-  const data = await axios.get(url);
-  console.log(data);
-};
